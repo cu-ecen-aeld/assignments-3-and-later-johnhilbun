e9b0121e422210ea7e3a4f0758674b2f0fbeab86
johnhilbun <john.hilbun@gmail.com>
2025-12-14 12:14:36 -0800

    Starter aesdsocket-start-stop.sh

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# JDH added Assignment 5 Part 2
+
+case "$1" in
+   start)
+      echo "Starting aesdsocket"
+      start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+      ;;
+   stop)
+      echo "Stopping aesdsocket"
+      start-stop-daemon -K -n aesdsocket
+      ;;
+   *)
+      echo "Usage: $0 (start|stop)"
+      exit 1
+esac
